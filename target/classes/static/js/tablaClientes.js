511bca7dd9d9924cf3d95ef3275c7ad9d8124d13
sebaandino <126474310+sebaandino@users.noreply.github.com>
2024-03-16 18:30:22 -0300

    se agrego listado de usuarios y clientes, tambien registro de usuarios

--- target/classes/static/js/tablaClientes.js
+++ target/classes/static/js/tablaClientes.js
@@ -7,7 +7,7 @@ cargarClientes();
 
 async function cargarClientes(){
 
-const request = await fetch('/api/all',{
+const request = await fetch('/api/clientes',{
 method: 'GET',
 headers:{
     'Accept':'application/json',
@@ -20,18 +20,8 @@ console.log(Clientes);
 
 let listadoHTML ='';
 for(let cliente of Clientes){
-let clienteHTML = ' <tr><td>'+cliente.id+'</td><td>'+cliente.nombre+'</td><td>'+cliente.apellido+'</td><td>'+cliente.dni+'</td><td>'+cliente.localidad+'</td><td><a href="#" onclick="{$eliminarCliente(cliente.id)}"  class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a> </td></tr>';
+let clienteHTML = ' <tr><td>'+cliente.id+'</td><td>'+cliente.nombre+'</td><td>'+cliente.apellido+'</td><td>'+cliente.dni+'</td><td>'+cliente.localidad+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a> </td></tr>';
 listadoHTML += clienteHTML;
 }
 document.querySelector('#tablaClientes tbody').innerHTML = listadoHTML;
-}
-
-async function eliminarCliente(id){
-  const request = await fetch('/api/delete/{id}',{
-    method: 'DELETE',
-    headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-    },
-    });
 }
